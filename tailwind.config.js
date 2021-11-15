38f065874c6de0b5af1d0c07c6f4164052897a84
boop_xyz <jm865731@gmail.com>
2021-11-15 11:19:21 -0500

    other stuff

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,26 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        blueGray: colors.blueGray,
+        trueGray: colors.trueGray,
+        warmGray: colors.warmGray,
+        amber: colors.amber,
+        lime: colors.lime,
+        orange: colors.orange,
+        emerald: colors.emerald,
+        teal: colors.teal,
+        cyan: colors.cyan,
+        sky: colors.sky,
+        violet: colors.purple,
+        fuchsia: colors.fuchsia,
+        rose: colors.rose,
+      },
+    },
   },
   variants: {
     extend: {
@@ -10,4 +28,4 @@ module.exports = {
     },
   },
   plugins: [],
-}
+};
